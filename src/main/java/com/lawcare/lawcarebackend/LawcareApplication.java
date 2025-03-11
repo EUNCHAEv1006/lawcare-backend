ddc62fc61ff81bcc5100c7d5b383067d51742d59
김은채 <ke808762@gmail.com>
2025-03-11 17:28:09 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/lawcare/lawcarebackend/LawcareApplication.java
@@ -0,0 +1,13 @@
+package com.lawcare.lawcarebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawcareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LawcareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lawcare/lawcarebackend/LawcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lawcare.lawcarebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawcareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
